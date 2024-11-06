c76e1efa2e2fee96e306b7454cddcfcfb490745b
arneyva <princethereal1416@gmail.com>
2024-11-06 11:17:26 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["inter"],
+        circular: ["Circular Std"],
+      },
+      colors: {
+        ungu: "#5D50C6",
+        jambon: "#F85E9F",
+        orange: "#FF5722",
+        grey: "#9E9E9E",
+      },
+      dropShadow: {
+        myshadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+      }
+    },
+  },
+  plugins: [],
+};
