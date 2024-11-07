d5372698679e0d8abc54980be769df8b392f7eb9
arneyva <princethereal1416@gmail.com>
2024-11-07 10:07:13 +0700

    update services

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,8 @@ module.exports = {
         grey: "#9E9E9E",
       },
       dropShadow: {
-        myshadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+        "myshadow": ["0px 4px 4px rgba(0, 0, 0, 0.25)"],
+        "xl-shadow": [" 0px 41px 89px 0px #0000001A"],
       }
     },
   },
